676bb788081d23b13de40a36d833af04e93a8642
Sirawit Loywirat <sloywirat@gmail.com>
2026-09-24 19:38:03 +0700

    fix: the order of morsecode array

--- morse_utils.cpp
+++ morse_utils.cpp
@@ -3,8 +3,8 @@
 const char LETTERS_NUMBERS[TOTAL_CHARECTERS] = {
   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
   'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-  'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3',
-  '4', '5', '6', '7', '8', '9'
+  'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4',
+  '5', '6', '7', '8', '9', '0'
 };
 
 const String MORSE_CODE[TOTAL_CHARECTERS] = {
@@ -34,7 +34,6 @@ const String MORSE_CODE[TOTAL_CHARECTERS] = {
   "-..-",  // X
   "-.--",  // Y
   "--..",  // Z
-  "-----"  // 0
   ".----", // 1
   "..---", // 2
   "...--", // 3
@@ -44,6 +43,7 @@ const String MORSE_CODE[TOTAL_CHARECTERS] = {
   "--...", // 7
   "---..", // 8
   "----.", // 9
+  "-----"  // 0
 };
 
 char morseDecode(String seq) {
